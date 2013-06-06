c66985bec013bf89cce0715bdb699638be4e8b57
Jakub Czuczwara <kontakt@wrizu.pl>
2013-06-06 14:09:30 +0200

    Update Core.php
    Zmiana informacji o autorach.

--- application/Gekosale/Core.php
+++ application/Gekosale/Core.php
@@ -1,10 +1,9 @@
 <?php
-
 /**
  * Gekosale, Open Source E-Commerce Solution
  * http://www.gekosale.pl
  *
- * Copyright (c) 2008-2012 Gekosale. Zabronione jest usuwanie informacji o licencji i autorach.
+ * Copyright (c) 2008-2013 WellCommerce sp. z o.o.. Zabronione jest usuwanie informacji o licencji i autorach.
  *
  * This library is free software; you can redistribute it and/or 
  * modify it under the terms of the GNU Lesser General Public
